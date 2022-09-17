cf814e29027c054a686a545efce4242a5fac1993
bonakulavinay <vinaybonakula5101@gmail.com>
2022-09-17 23:34:06 +0530

    playing with patterns

---
+++ playing with patterns/Patterns/src/com/corejavaexamples/Square.java
@@ -0,0 +1,19 @@
+package com.corejavaexamples;
+
+public class Square {
+
+	public static void main(String[] args) {
+		for(int x=1;x<=3;x++) {
+			for(int y=1;y<=3;y++) {
+				
+					System.out.print("*");
+				}
+				System.out.println();
+				
+				
+			}
+		}
+
+	}
+
+
